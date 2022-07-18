7d113ba6269c95ffa34f763ca52cf80088a461b9
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-07-18 16:36:53 +0200

    Be able to inhibit SSE instructions via a define
    In some context (like the cross-compilation), we want to compile mirage-crypto
    for x86_64 architecture without SSE. This patch allows to inhibit the choice of
    accelerated instructions when __MIRAGE_CRYPTO_NO_ACCELERATE__ is defined.

--- config/cfg.ml
+++ config/cfg.ml
@@ -1,35 +1,38 @@
+let std_flags = [ "--std=c11"; "-Wall"; "-Wextra"; "-O3" ]
+
 let () =
   let c = Configurator.V1.create "mirage-crypto" in
   let ccomp_type_opt = Configurator.V1.ocaml_config_var c "ccomp_type" in
   let arch =
     let defines =
-      Configurator.V1.C_define.import
-        c
-        ~includes:[]
-        [("__x86_64__", Switch); ("__i386__", Switch); ("__powerpc64__", Switch);
-         ("__s390x__", Switch); ("__aarch64__", Switch);
-         ("_WIN64", Switch); ("_WIN32", Switch)]
+      Configurator.V1.C_define.import c ~includes:[]
+        [
+          ("__MIRAGE_CRYPTO_NO_ACCELERATE__", Switch);
+          ("__x86_64__", Switch);
+          ("__i386__", Switch);
+          ("__powerpc64__", Switch);
+          ("__s390x__", Switch);
+          ("__aarch64__", Switch);
+          ("_WIN64", Switch);
+          ("_WIN32", Switch);
+        ]
     in
     match defines with
-    | (_, Switch true) :: _ -> `x86_64
-    | _ :: (_, Switch true) :: _ -> `x86
-    | _ :: _ :: (_, Switch true) :: _ -> `ppc64
-    | _ :: _ :: _ :: (_, Switch true) :: _ -> `s390x
-    | _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `arm64
-    | _ :: _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `x86_64
-    | _ :: _ :: _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `x86
+    | (_, Switch true) :: _ -> `none
+    | _ :: (_, Switch true) :: _ -> `x86_64
+    | _ :: _ :: (_, Switch true) :: _ -> `x86
+    | _ :: _ :: _ :: (_, Switch true) :: _ -> `ppc64
+    | _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `s390x
+    | _ :: _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `arm64
+    | _ :: _ :: _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `x86_64
+    | _ :: _ :: _ :: _ :: _ :: _ :: _ :: (_, Switch true) :: _ -> `x86
     | _ -> `unknown
   in
   let os =
     let defines =
-      Configurator.V1.C_define.import
-        c
-        ~includes:[]
-        [("__APPLE__", Switch)]
+      Configurator.V1.C_define.import c ~includes:[] [ ("__APPLE__", Switch) ]
     in
-    match defines with
-    | (_, Switch true) :: _ -> `macos
-    | _ -> `unknown
+    match defines with (_, Switch true) :: _ -> `macos | _ -> `unknown
   in
   let accelerate_flags =
     match arch, ccomp_type_opt with
@@ -55,14 +58,14 @@ let () =
     | (`ppc64, _) | (`s390x, _) -> [ "-Wno-stringop-overflow"; "-Werror" ]
     | _ -> [ "-Werror" ]
   in
-  let no_instcombine_on_macos = match arch, os with
+  let no_instcombine_on_macos =
+    match (arch, os) with
     | `arm64, `macos ->
-      let res = Configurator.V1.Process.run c "cc" ["-dumpversion"] in
-      if String.trim res.stdout = "14.0.3" then
-        ["-mllvm"; "--instcombine-max-iterations=0"]
-        (* macOS instcombine miscompilation with clang 14.0.3 *)
-      else
-        []
+        let res = Configurator.V1.Process.run c "cc" [ "-dumpversion" ] in
+        if String.trim res.stdout = "14.0.3" then
+          [ "-mllvm"; "--instcombine-max-iterations=0" ]
+          (* macOS instcombine miscompilation with clang 14.0.3 *)
+        else []
     | _ -> []
   in
   let flags = std_flags @ no_instcombine_on_macos @ ent_flags in
